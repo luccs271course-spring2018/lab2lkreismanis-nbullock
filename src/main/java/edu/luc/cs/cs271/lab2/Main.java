8044e687650866f07ce69e977376d57135d20a3a
Larisa Kreismanis <lkreismanis@luc.edu>
2017-09-25 13:29:59 +0000

    Main.java: added format() method

--- src/main/java/edu/luc/cs/cs271/lab2/Main.java
+++ src/main/java/edu/luc/cs/cs271/lab2/Main.java
@@ -50,12 +50,14 @@ public class Main {
       System.out.println("Found!");
       final int pos = index.get();
       final Team team = array[pos];
-      // TODO DRY - eliminate this code duplication
+      /* TODO DRY - eliminate this code duplication
       System.out.println("Name: " + team.getName());
       System.out.println("Head coach: " + team.getHeadcoach());
       System.out.println("Funding: " + team.getFunding());
       System.out.println("Array index: " + pos);
       System.out.println("Ranking: " + (pos + 1));
+      */
+      team.format();
     } else {
       System.out.println("Not Found!");
     }
@@ -67,14 +69,26 @@ public class Main {
       System.out.println("Found!");
       final int pos = index.get();
       final Team team = list.get(pos);
-      // TODO DRY - eliminate this code duplication
+      /* TODO DRY - eliminate this code duplication
       System.out.println("Name: " + team.getName());
       System.out.println("Head coach: " + team.getHeadcoach());
       System.out.println("Funding: " + team.getFunding());
       System.out.println("Array index: " + pos);
       System.out.println("Ranking: " + (pos + 1));
+      */
+      team.format();
     } else {
       System.out.println("Not Found!");
     }
   }
+  
+  public static void format(){
+    System.out.println("Name: " + team.getName());
+    System.out.println("Head coach: " + team.getHeadcoach());
+    System.out.println("Funding: " + team.getFunding());
+    System.out.println("Array index: " + pos);
+    System.out.println("Ranking: " + (pos + 1));
+  }
+  
+  
 }
